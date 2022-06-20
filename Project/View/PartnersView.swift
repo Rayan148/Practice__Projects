112bb87463d87c44d09699fa18e05fa1a1e2ef59
Rayan Shrestha <rayan.shrestha@ebpearls.com>
2022-06-20 17:01:15 +0545

    feat: added partners view.

---
+++ Project/View/PartnersView.swift
@@ -0,0 +1,20 @@
+//
+//  PartnersView.swift
+//  Project
+//
+//  Created by Ebpearls on 20/06/2022.
+//
+
+import UIKit
+
+class PartnersView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
